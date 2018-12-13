e09d2ac275c37a5f646bd2b6ddf9e9b1a4b48ad3
Arjen de Bruijn <Arjen@w3s.home>
2018-12-13 14:01:39 +0100

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: [
+      './src/index.js',
+      './src/main.scss'
+    ]
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+            "style-loader", // creates style nodes from JS strings
+            "css-loader", // translates CSS into CommonJS
+            "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      }
+    ]
+  },
+  plugins: [
+      new webpack.ProvidePlugin({
+          _: 'underscore'
+      })
+  ],
+}
